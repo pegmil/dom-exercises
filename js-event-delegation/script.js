73f326125308bed2d0a49fee38eea64b4b9ce9f9
pegmil <pjmilos@comcast.net>
2026-06-24 13:23:23 +0000

    Playing with the DOM with exercises

---
+++ js-event-delegation/script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu');
+
+menu.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case 'home':
+            console.log('Home menu item was clicked');
+            break;
+        case 'dashboard':
+            console.log('Dashboard menu item was clicked');
+            break;
+        case 'report':
+            console.log('Report menu item was clicked');
+            break;
+    }
+});
